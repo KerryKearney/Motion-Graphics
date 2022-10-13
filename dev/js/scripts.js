edebaa3affa35a12ebdb3fdc2e6a526e6ade3e57
KerryKearney <kpk54@drexel.edu>
2022-10-12 21:45:07 -0400

    gsap commented out

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 
-gsap.from("#line-1", {duration:1,y:-200,alpha:0})
-gsap.from("#line-2", {duration:1,y:-100,alpha:0,delay:0.5})
+// gsap.from("#line-1", {duration:1,y:-200,alpha:0})
+// gsap.from("#line-2", {duration:1,y:-100,alpha:0,delay:0.5})
 
-gsap.from("#pencil-btn", {duration:1, y:200, alpha:0, delay:1})
+// gsap.from("#pencil-btn", {duration:1, y:200, alpha:0, delay:1})
+
