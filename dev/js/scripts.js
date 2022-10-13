054bbd22f212b05bcd3a292c44b6935a5501e34e
KerryKearney <kpk54@drexel.edu>
2022-10-12 22:23:37 -0400

    timeline+function

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,16 @@ import { gsap } from "gsap";
 
 // gsap.from("#pencil-btn", {duration:1, y:200, alpha:0, delay:1})
 
+
+function heroanimation(){
+    var tl = gsap.timeline();
+
+    tl.from("#line-1", {duration:1,y:-200,alpha:0})
+        .from("#line-2", {duration:1,y:-100,alpha:0},"-=0.5")
+        .from("#pencil-btn", {duration:1, y:200, alpha:0})
+    
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroanimation());
