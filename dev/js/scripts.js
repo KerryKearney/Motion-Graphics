a2027fdc793b7ab4deb1bdd46da5a75f1e24b43c
KerryKearney <kpk54@drexel.edu>
2022-10-24 23:13:04 -0400

    ..

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,14 +20,34 @@ function heroanimation(){
 }
 
 function section1animation(){
-    var tl = gsap.timeline({scrollTrigger: {trigger:"#section-1", scrub:true, markers:true, end:"top 20%", start:"top 30%"}});
+    var tl = gsap.timeline({scrollTrigger: {trigger:"#section-1", scrub:true, markers:false, end:"top 20%", start:"top 50%"}});
 
-    tl.from("#definition", {duration:5, x:50, alpha:0});
+    tl.from("#definition", {duration:5, y:-50, alpha:0})
+        .from("#body p", {duration:10, alpha:0, stagger:1})
 
     return tl;
 }
 
+function section2animation(){
+    var tl = gsap.timeline({scrollTrigger: {trigger:"#section-2", scrub:true, markers:false, end:"top 50%", start:"top 80%"}});
+
+    tl.from("#phrase-1", {duration:5, y:-50, alpha:0})
+        .from("#phrase-2", {duration:5, alpha:0, scale:0})
+
+    return tl;
+}
+
+function section3animation(){
+    var tl = gsap.timeline({scrollTrigger: {trigger:"#section-3", scrub:true, markers:true, end:"top 20%", start:"top 80%"}});
+
+    tl.from("#section-3 img", {duration:5, alpha:0, stagger:1})
+        .from("#section-3 .card-title", {duration:5, alpha:0, scale:0, stagger:1})
+
+    return tl;
+}
 
 var mainTL = gsap.timeline();
 mainTL.add(heroanimation())
     .add(section1animation())
+    .add(section2animation())
+    .add(section3animation())
