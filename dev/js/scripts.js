874ee26a7976e873cd6daebfc48a0bc227f6d54f
KerryKearney <kpk54@drexel.edu>
2022-10-31 14:50:44 -0400

    mess around

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -19,15 +19,16 @@ function simplemotion(){
     // gsap.set("#C5",{transformOrigin:"center"})
 
     var tl = gsap.timeline()
-    tl.from(".circle",{scale:0, duration:0.25, stagger:0.25}, "playCircle")
-    .fromTo("#Curves", {drawSVG:"0%"}, {duration:1, drawSVG:"100% 0%"}, "drawCurves")
+    tl.from(".circle",{scale:0, duration:0.25, stagger:0.25}, "playmotion")
+    .fromTo("#Curves", {drawSVG:"0%"}, {duration:1, drawSVG:"100% 0%"}, "playmotion")
 
     return tl;
 }
 
 function patternmotion(){
     var tl= gsap.timeline()
-
+    tl.from(".odd",{duration:5, ease:'bounce', rotate:360, drawSVG:"100%", stagger:0.25, transformOrigin:"center"}, "playpattern")
+    .from(".even",{duration:5, ease:'bounce', rotate:360, drawSVG:"0% -100%", stagger:0.25, transformOrigin:"center"}, "playpattern")
     return tl;
 }
 
