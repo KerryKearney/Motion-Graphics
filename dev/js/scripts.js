6f148d9d67ea8a0ec4de3143ef8f031692d00f31
KerryKearney <kpk54@drexel.edu>
2022-11-02 20:28:24 -0400

    finished UI motion

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -19,22 +19,35 @@ function simplemotion(){
     // gsap.set("#C5",{transformOrigin:"center"})
 
     var tl = gsap.timeline()
-    tl.from(".circle",{scale:0, duration:0.25, stagger:0.25}, "playmotion")
+    tl.fromTo(".circle",{scale:0}, {scale:1, duration:0.25, stagger:0.25}, "playmotion")
     .fromTo("#Curves", {drawSVG:"0%"}, {duration:1, drawSVG:"100% 0%"}, "playmotion")
 
     return tl;
 }
 
 function patternmotion(){
-    var tl= gsap.timeline()
-    tl.from(".odd",{duration:5, ease:'bounce', rotate:360, drawSVG:"100%", stagger:0.25, transformOrigin:"center"}, "playpattern")
+    var tl = gsap.timeline()
+    tl.from(".odd",{duration:5, ease:'yoyo', rotate:360, drawSVG:"100%", stagger:0.25, transformOrigin:"center"}, "playpattern")
     .from(".even",{duration:5, ease:'bounce', rotate:360, drawSVG:"0% -100%", stagger:0.25, transformOrigin:"center"}, "playpattern")
+
+    return tl;
+}
+
+function UImotion(){
+    var tl = gsap.timeline()
+    tl.fromTo("#button",{scale:0}, {scale:"100%", duration:0.25})
+    .to("#cart",{x:"170%"},"cartroll")
+    .to(".letter",{x:"25%", alpha:0},"cartroll")
+    .fromTo("#circle",{drawSVG:"0"}, {drawSVG:"100%"})
+    .fromTo("#check",{drawSVG:"0"}, {drawSVG:"100%"})
+
     return tl;
 }
 
 var mainTL = gsap.timeline();
 mainTL.add(simplemotion())
-    .add(patternmotion());
+    .add(patternmotion())
+    .add(UImotion());
 
 
 
