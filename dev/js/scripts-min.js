d2a4d3d3bf6ddd711fafc3550dc79e646f38d404
KerryKearney <kpk54@drexel.edu>
2022-11-14 18:56:36 -0500

    svg update

---
+++ dev/js/scripts-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";import{GSDevTools}from"gsap/GSDevTools";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";import{MotionPathPlugin}from"gsap/MotionPathPlugin";gsap.registerPlugin(GSDevTools,DrawSVGPlugin,MotionPathPlugin),GSDevTools.create();
