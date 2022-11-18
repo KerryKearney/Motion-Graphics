c4a0d36d62fbea7a2d3e4aa1fe8a7ec58686f022
KerryKearney <kpk54@drexel.edu>
2022-11-18 18:36:12 -0500

    isolating first target
    -hidden class

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,7 +5,27 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
+function uhMotion(){
+        var tl = gsap.timeline()
+
+        // draw u
+        .fromTo("#uhlive-u", {duration:0.5, DrawSVG:"0% 10%"}, {DrawSVG:"100%"})
+
+        // draw h stem
+        
+
+        // draw h shoulder
 
 
+        // dots scale up, then retract
+        
+
+        return tl;
+
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(uhMotion())
 
 GSDevTools.create();
