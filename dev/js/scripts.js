b552d1788de8ef28cf6398402a9cd8b10bea2cbd
KerryKearney <kpk54@drexel.edu>
2022-11-21 19:15:17 -0500

    firesidechat-update
    -update figma

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,23 +9,75 @@ function uhMotion(){
         var tl = gsap.timeline()
 
         // draw u
-        .fromTo("#uhlive-u", {duration:0.5, DrawSVG:"0% 10%"}, {DrawSVG:"100%"})
+        tl.from("#uhlive-u", {duration:0.5, drawSVG: 0})
 
         // draw h stem
-        
+        // .from("#uhlive-h-stem", {duration:0.5, x:"0%"})
 
         // draw h shoulder
 
 
         // dots scale up, then retract
-        
+
 
         return tl;
 
 }
 
+function liveMotion(){
+        var tl = gsap.timeline()
+
+        // morph dot to bubble
+
+
+        // lengthen l
+
+
+        // lengthen i
+
+
+        // unfold v
+
+
+        // scale up e
+
+
+        // scale up copyright mark
+
+
+        return tl;
+}
+
+function subtitleMotion(){
+        var tl = gsap.timeline()
+
+        // fade-in
+
+
+        // type in
+
+
+        // lengthen line
+
+
+        // live! type in
+
+
+        return tl;
+}
+
+function uhliveMotion(){
+        var tl = gsap.timeline()
+
+        
+
+        return tl;
+}
 
 var mainTL = gsap.timeline();
 mainTL.add(uhMotion())
+.add(liveMotion())
+.add(subtitleMotion())
+.add(uhliveMotion())
 
 GSDevTools.create();
