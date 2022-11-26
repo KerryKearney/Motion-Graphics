dffbc8b7b28d2161c854f637f8536363c1c10a06
KerryKearney <kpk54@drexel.edu>
2022-11-25 21:56:27 -0500

    attempt1-clippath

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,5 +5,23 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
+gsap.set("#clip1", {transformOrigin:"center"});
+
+function swirlAnimation(){
+        var tl = gsap.timeline();
+        tl.from("#clip1", {duration:1 , rotate:180})
+
+        return tl;
+}
+
+function blinkAnimation(){
+        var tl = gsap.timeline();
+
+        return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(swirlAnimation())
+.add(blinkAnimation())
 
 GSDevTools.create();
