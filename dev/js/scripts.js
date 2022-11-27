03dd19536b2dae44d1d5216f5b701ecd9180e207
KerryKearney <kpk54@drexel.edu>
2022-11-26 22:57:41 -0500

    blink attempt1
    using morph svg and clipping path

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,11 +5,29 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
-function swirlAnimation(){
+gsap.set("#outter",{transformOrigin:"center"});
+
+function outterAnimation(){
         var tl = gsap.timeline();
 
-        tl.from("#outter-line", {duration:1, drawSVG:0})
-        .from("#inner-line", {duration:1, drawSVG:0})
+        tl.from("#outter-line", {duration:1.5, drawSVG:0}, "rotateoutter")
+        .to("#outter", {duration:1, rotate:-360}, "rotateoutter")
+
+        return tl;
+}
+
+function innerAnimation(){
+        var tl = gsap.timeline();
+
+        tl.from("#inner-line", {duration:1, drawSVG:0})
+
+        return tl;
+}
+
+function crunchyrollAnimation(){
+        var tl = gsap.timeline();
+
+        tl.from("#crunchyroll", {duration: 2, alpha:0, y:-30, stagger:0.5})
 
         return tl;
 }
@@ -17,11 +35,33 @@ function swirlAnimation(){
 function blinkAnimation(){
         var tl = gsap.timeline();
 
+
+
+        return tl;
+}
+
+function sparkleAnimation(){
+        var tl = gsap.timeline();
+
+
+
+        return tl;
+}
+
+function openAnimation(){
+        var tl = gsap.timeline();
+
+
+
         return tl;
 }
 
 var mainTL = gsap.timeline();
-mainTL.add(swirlAnimation())
+mainTL.add(outterAnimation())
+.add(innerAnimation(), "-=.75")
+.add(crunchyrollAnimation(), "-=.5")
 .add(blinkAnimation())
+.add(sparkleAnimation())
+.add(openAnimation())
 
 GSDevTools.create();
