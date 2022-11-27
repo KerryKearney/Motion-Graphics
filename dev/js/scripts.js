2fcdc7c9b5796ff9e8e2fa4adba4be176a785041
KerryKearney <kpk54@drexel.edu>
2022-11-27 17:57:45 -0500

    L-bubble Morph

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,13 +2,19 @@ import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
+
+
 
 gsap.set("#dot1", {transformOrigin: "center"});
 gsap.set("#dot2", {transformOrigin: "center"});
 gsap.set("#bubble-L", {transformOrigin: "center"});
 gsap.set("#uhlive-e", {transformOrigin: "center"});
+gsap.set("#uhlive-v-left", {transformOrigin:"right bottom"});
+gsap.set("#uhlive-v-right", {transformOrigin:"left bottom"});
 gsap.set("#copyright-r", {transformOrigin: "center"});
 gsap.set("#copyright-circle", {transformOrigin: "center"});
 
@@ -16,20 +22,20 @@ function uhMotion(){
         var tl = gsap.timeline()
 
         // draw u
-        tl.from("#uhlive-u", {duration:0.5 , drawSVG: "0"})
+        tl.from("#uhlive-u", {duration:0.5 , drawSVG: "0"}, "uhAnimation")
 
         // draw h stem
-        .from("#uhlive-h-stem", {duration:0.5 , drawSVG: "0"})
+        .from("#uhlive-h-stem", {duration:0.5 , drawSVG: "0"}, "uhAnimation")
 
         // draw h shoulder
-        .from("#uhlive-h-shoulder", {duration:0.5 , drawSVG: "0"})
+        .from("#uhlive-h-shoulder", {duration:0.5 , drawSVG: "0"}, "uhAnimation")
 
         // dots scale up, then retract
         .from("#dot1", {duration:0.25 , scale:0}, "dotsAnimation")
         .fromTo("#dot2", {duration:0.25 , scale:0 , x:"32"}, {scale:1 , x:"32"}, "dotsAnimation")
-        .fromTo("#bubble-L", {duration:0.25 , scale:0 , x:"64"}, {scale:1 , x:"64"}, "dotsAnimation")
+        .fromTo("#dot3", {duration:0.25 , scale:0 , x:"64"}, {scale:1 , x:"64"}, "dotsAnimation")
         .to("#dot2", {x:"0"}, "dotsReturn")
-        .to("#bubble-L", {x:"0"}, "dotsReturn")
+        .to("#dot3", {x:"0"}, "dotsReturn")
 
         return tl;
 
@@ -39,18 +45,18 @@ function liveMotion(){
         var tl = gsap.timeline()
 
         // morph dot to bubble
-        
+        tl.to("#dot3", {duration:0.5, morphSVG:"#bubble-L"}, "liveAnimation")
 
         // lengthen l
-        tl.from("#uhlive-l", {duration:0.5 , drawSVG:"0"}, "liveAnimation")
+        .from("#uhlive-l", {duration:0.5 , drawSVG:"0"}, "liveAnimation")
 
         // lengthen i
         .from("#uhlive-i", {duration:0.5 , drawSVG:"0"}, "liveAnimation")
 
         // unfold v
         .fromTo("#uhlive-v-base", {duration:0.1, alpha:0}, {alpha:1}, "liveAnimation")
-        .fromTo("#uhlive-v-right", {duration:0.5 , drawSVG:"0"}, {drawSVG:"100%"}, "liveAnimation")
-        .fromTo("#uhlive-v-left", {duration:0.5 , drawSVG:"0"}, {drawSVG:"100%"}, "liveAnimation")
+        .fromTo("#uhlive-v-right", {duration:1 , drawSVG:"0", rotate:-30}, {drawSVG:"100%", rotate:0}, "liveAnimation")
+        .fromTo("#uhlive-v-left", {duration:1 , drawSVG:"0", rotate:30}, {drawSVG:"100%", rotate:0}, "liveAnimation")
 
         // scale up e
         .from("#uhlive-e", {duration:0.5 , scale:0}, "liveAnimation")
